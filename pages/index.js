4b25f7eef9fdc7b12c379e4081b095315813c24b
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2022-10-31 18:22:10 +0100

    finished the hulu-clone

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
-
-export default function Home() {
+import Results from "../components/Results"
+import request from "../utils/request"
+export default function Home({results}) {
+  //console.log(results);
   return (
     <div>
       <Head>
@@ -16,6 +18,22 @@ export default function Home() {
       {/* Navbar Component */}
       <Navbar />
       {/* Results Component */}
+      <Results results={results}/>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+
+  const requests = await fetch(
+      `https://api.themoviedb.org/3${request[genre]?.url || request.fetchTrending.url
+    }`
+  ).then(res => res.json())
+
+  return {
+    props: {
+      results: requests.results
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   mode: 'jit',
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px"
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 }
